60ec95c7add10ea7fc3f12a8956a3495d6627da4
dmunofer <diego.fmz@hotmail.com>
2021-10-29 10:22:49 +0200

    cambio bitcoin

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,7 +1,8 @@
-def bitcoin1(m):
-    m = input('Dime la movida crack')
-    if m > 30000:
-        print('andamo jodido loko')
-    else:
-        print('montao en el dolar manin')
-        
+#Tarea 1 Bitcoin
+
+m = int(input('Dime la movida crack:'))
+if m < 30000:
+    print('andamo jodido loko')
+else:
+    print('montao en el dolar manin')
+
