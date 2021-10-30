fa9021a7bd4a57513b1f5f7f4c58de9ace9adae9
dmunofer <diego.fmz@hotmail.com>
2021-10-30 19:54:46 +0200

    hasta la tarea 6

---
+++ Tarea2.py
@@ -0,0 +1,14 @@
+#Tarea 2
+
+from colorama import Fore
+print(f'Hello world{Fore.BLUE}')
+print("Hello world")
+
+# We can use the extension "Colorama" to print in different colors
+
+
+
+
+
+
+
--- ejercicio1.py
+++
@@ -1,8 +0,0 @@
-#Tarea 1 Bitcoin
-
-m = int(input('Dime la movida crack:'))
-if m < 30000:
-    print('andamo jodido loko')
-else:
-    print('montao en el dolar manin')
-
